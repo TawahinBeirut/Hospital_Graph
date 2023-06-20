83e149d76e99dcc18934c6bdce07e2d85f88b242
GHERIBI ABD-EL-RAHMEN p2200699 <abd-el-rahmen.gheribi@etu.univ-lyon1.fr>
2023-06-20 08:27:11 +0000

    Upload New File

---
+++ SAE/Models/GrapheException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package sae.Model;
+
+/**
+ * Exception (rarement appelée)
+ * @author gheri
+ */
+public class GrapheException extends Exception{
+
+    /**
+     * Creates a new instance of <code>GrapheException</code> without detail
+     * message.
+     */
+    public GrapheException() {
+        System.out.println("Erreur dans le Graphe innatendue");
+    }
+}
